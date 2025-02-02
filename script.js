ea17d172dc4e610091afd4711a82ed13ad47e15d
lokeshpasupuleti-sketch <lokypowerstar@gmail.com>
2025-02-02 20:01:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Open Lightbox
 document.querySelectorAll('.gallery-img').forEach(item => {
     item.addEventListener('click', (event) => {
         const fullSizeUrl = item.getAttribute('data-full');
